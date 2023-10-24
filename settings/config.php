6d874d9dfc7181ff94687e4f5a933e92573809f6
caiocmb <caiocmb@hotmail.com>
2023-10-23 21:19:19 -0300

    Tratamentod e erros

--- settings/config.php
+++ settings/config.php
@@ -21,6 +21,8 @@ ini_set('display_errors',1);
 ini_set('display_startup_erros',1);
 error_reporting(E_ALL);*/
 
+error_reporting(0);
+
 require('../global.php');
 
 include($_ENV['DIR_APP']."/settings/seguranca.php");
